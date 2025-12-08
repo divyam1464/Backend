e842c1fd1516a9390f0e1b283b981243f67a700b
Divyam Agarwal <divyam7246@gmail.com>
2025-12-08 17:37:23 +0530

    change done

---
+++ Express.js/controller.js
@@ -0,0 +1,6 @@
+export const userLogin = (req, res) => {
+  res.send("this is user login route");
+};
+export const userSignup = (req, res) => {
+  res.send("this is user signup route");
+};
---
+++ Express.js/index.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+
+import express from 'express';
+import { userLogin, userSignup } from './controller.js';
+
+const app = express();
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello, World");
+});
+
+app.get('/user/login',userLogin);
+app.get('/user/signup',userSignup);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
