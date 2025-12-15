1a0272e8fcea18380778b8603f048b07fd5dc1a0
Divyam Agarwal <divyam7246@gmail.com>
2025-12-15 16:17:46 +0530

    changes

--- Express.js/index.js
+++ Express.js/index.js
@@ -16,24 +16,35 @@ app.get("/", (req, res) => {
 
 // Saving data in mongodb
 app.post("/person", async (req, res) => {
-  const { email, name, age } = req.body;
-  const newPerson = new Person({
-    name,
-    age,
-    email,
-  });
-  await newPerson.save();
-  console.log(newPerson);
-  res.send("Person added");
+  try {
+    const { email, name, age } = req.body;
+    const newPerson = new Person({
+      name,
+      age,
+      email,
+    });
+    await newPerson.save();
+    console.log(newPerson);
+    res.send("Person added");
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 // Updating data in mongodb
-app.put("/person", async (req, res) => {
-  const { id } = req.body;
-  const personData = await Person.findByIdAndUpdate(id, { age: 28 });
+// app.put("/person", async (req, res) => {
+//   const { id } = req.body;
+//   const personData = await Person.findByIdAndUpdate(id, { age: 99 });
 
-  console.log(personData);
-  res.send("Person Updated");
+//   console.log(personData);
+//   res.send("Person Updated");
+// });
+
+// Deletion Task
+app.delete("/person:id", async (req, res) => {
+  const { id } = req.params;
+  await Person.findByIdAndDelete(id);
+  res.send("User Deleted");
 });
 
 app.listen(PORT, () => {
--- Express.js/models/person.js
+++ Express.js/models/person.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  email: String,
-});
+const personSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    userOrder: { type: Object, default: {} },
+  },
+  {
+    timestamps: true,
+    minimize: false,
+  }
+);
 
 export const Person = mongoose.model("Person", personSchema);
-
-
