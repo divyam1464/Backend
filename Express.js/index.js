cf5403c95bf6be0f15efe3490f3255c2d0742d68
Divyam Agarwal <divyam7246@gmail.com>
2025-12-15 17:21:02 +0530

    changes

--- Express.js/index.js
+++ Express.js/index.js
@@ -1,50 +1,14 @@
 // const express = require("express");
-
 import express from "express";
-import { connectDB } from "./config/db.js";
-import { Person } from "./models/person.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3000;
-app.use(express.json());
-
-await connectDB();
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
+  res.cookie("name", "express-app");
   res.send("Hello Express");
-});
-
-// Saving data in mongodb
-app.post("/person", async (req, res) => {
-  try {
-    const { email, name, age } = req.body;
-    const newPerson = new Person({
-      name,
-      age,
-      email,
-    });
-    await newPerson.save();
-    console.log(newPerson);
-    res.send("Person added");
-  } catch (error) {
-    res.send(error.message);
-  }
-});
-
-// Updating data in mongodb
-// app.put("/person", async (req, res) => {
-//   const { id } = req.body;
-//   const personData = await Person.findByIdAndUpdate(id, { age: 99 });
-
-//   console.log(personData);
-//   res.send("Person Updated");
-// });
-
-// Deletion Task
-app.delete("/person:id", async (req, res) => {
-  const { id } = req.params;
-  await Person.findByIdAndDelete(id);
-  res.send("User Deleted");
 });
 
 app.listen(PORT, () => {
