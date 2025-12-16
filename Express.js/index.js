5d27dd91dc44cd2801877a95a05ea9670a0b3798
Divyam Agarwal <divyam7246@gmail.com>
2025-12-16 15:26:09 +0530

    changes

--- Express.js/index.js
+++ Express.js/index.js
@@ -1,14 +1,48 @@
 // const express = require("express");
-import express from "express";
 import cookieParser from "cookie-parser";
+import express from "express";
+import session from "express-session";
 
 const app = express();
 const PORT = 3000;
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "sample-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+const users = [];
 
 app.get("/", (req, res) => {
-  res.cookie("name", "express-app");
   res.send("Hello Express");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  users.push({
+    username,
+    password,
+  });
+  res.send("User Registered");
+});
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username);
+  if (!user || password !== user.password) {
+    return res.send("Not Authorized");
+  }
+  req.session.user = user;
+  res.send("User Login");
+});
+
+app.get("/dashboard", (req, res) => {
+  if (!req.session.user) {
+    return res.send("Unauthorized");
+  }
+  res.send(`Welcome, ${req.session.user.username}`);
 });
 
 app.listen(PORT, () => {
